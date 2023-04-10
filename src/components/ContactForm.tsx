7fb60717dc5e327ccb628153acf184558ba96fc2
Istiak Ahmad Polash <58633156+Polashahmad01@users.noreply.github.com>
2023-04-10 17:18:11 +0600

    fix contact form text color issue

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -22,7 +22,7 @@ export const ContactForm = () => {
           <div className="flex flex-col mb-4">
             <label
               htmlFor="name"
-              className="mb-2 text-xs font-medium text-gray-900 dark:text-white sm:text-base"
+              className="mb-2 text-xs font-medium dark:text-white sm:text-base"
             >
               Name
             </label>
@@ -40,7 +40,7 @@ export const ContactForm = () => {
           <div className="flex flex-col mb-4">
             <label
               htmlFor="email"
-              className="mb-2 text-xs font-medium text-gray-900 dark:text-white md:text-base"
+              className="mb-2 text-xs font-medium dark:text-white md:text-base"
             >
               Email
             </label>
@@ -58,7 +58,7 @@ export const ContactForm = () => {
           <div className="flex flex-col mb-4">
             <label
               htmlFor="message"
-              className="mb-2 text-xs font-medium text-gray-900 dark:text-white md:text-base"
+              className="mb-2 text-xs font-medium dark:text-white md:text-base"
             >
               Message
             </label>
@@ -77,7 +77,7 @@ export const ContactForm = () => {
           </div>
           <div className="flex flex-col my-6">
           <button
-            className="bg-gray-50 border py-2 md:py-3 px-4 border-gray-300 text-gray-900 text-xs md:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 cursor-pointer hover:text-indigo-600"
+            className="bg-gray-50 border py-2 md:py-3 px-4 border-gray-300 text-xs md:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 cursor-pointer hover:text-indigo-600"
             style={{ backgroundColor: "#181818"}}
           >
             Send Message
