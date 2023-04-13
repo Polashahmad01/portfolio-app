227ca337dd8e3fa53eaf312cf20836111686257e
Polashahmad01 <dev.polashahmad@gmail.com>
2023-04-13 21:01:48 +0600

    improve review slider

--- src/components/PeopleSay.tsx
+++ src/components/PeopleSay.tsx
@@ -4,11 +4,16 @@ export const PeopleSay = (props: PeopleSayProps) => {
   const { descriptionPartOne, descriptionPartTwo, imageUrl, imageAlt, nameOfReviewer, role, socialProfile } = usePeopleSay(props)
 
   return (
-    <div className="flex flex-col w-4/5 mx-auto gap-8 justify-center h-full">
-      <div className="bg-white text-black mx-4 px-4 py-6 rounded-md">
-        <p className="mb-4">{descriptionPartOne}</p>
-        <p>{descriptionPartTwo}</p>
-      </div>
+    <div className="flex flex-col w-4/5 mx-auto gap-10 justify-center h-full">
+      <figure className="bg-white text-black mx-4 px-4 py-6 rounded-md relative">
+        <blockquote>
+          <div className="h-6 text-5xl text-left text-gray-600">“</div>
+          <p className="mb-4 mx-4 italic">{descriptionPartOne}</p>
+          <p className="italic mx-4">{descriptionPartTwo}</p>
+          <div className="h-3 text-5xl text-right text-gray-600">”</div>
+          <div style={{ top: "100%", width: 0, height: 0, borderLeft: "0 solid transparent", borderRight: "25px solid transparent", borderTop: "25px solid #fff", margin: 0, position: "absolute", left: "40px" }} />
+        </blockquote>
+      </figure>
       <div className="flex items-center gap-6 mx-4">
         <div className="h-20 w-20">
           <img
