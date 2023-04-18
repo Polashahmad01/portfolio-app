8598ec14123d7925ee9fb5b18a2601944649e83c
Polashahmad01 <dev.polashahmad@gmail.com>
2023-04-18 17:38:28 +0600

    add abstract icons

--- src/components/Comfort.tsx
+++ src/components/Comfort.tsx
@@ -9,11 +9,11 @@ export const Comfort = (props: ComfortProps) => {
 
   return (
     <div className="mt-4 sm:mt-10 rounded-2xl p-5 transition mb-12 sm:mb-0" style={{ backgroundColor: "#181818"}}>
-      <div className="mb-4">
-        <img className="h-16 w-16 m-auto sm:m-0" src={icon} alt={title} />
+      <div className="mb-2">
+        <img className="h-32 w-32 m-auto sm:m-0" src={icon} alt={title} />
       </div>
-      <h2 className="text-lg font-bold mb-4 text-center sm:text-left  sm:text-2xl">{title}</h2>
-      <p className="text-base leading-6 pb-4 text-center sm:text-left">
+      <h2 className="text-lg font-bold mb-4 ml-0 text-center sm:ml-5 sm:text-left  sm:text-2xl">{title}</h2>
+      <p className="text-base leading-6 pb-4 ml-0 text-center sm:ml-5 sm:text-left">
         {description}
       </p>
     </div>
--- src/components/Tools.tsx
+++ src/components/Tools.tsx
@@ -9,8 +9,8 @@ export const Tools = (props: ToolsProps) => {
 
   return (
     <div className="mt-4 sm:mt-10 rounded-2xl p-5 transition mb-12 sm:mb-0" style={{ backgroundColor: "#181818"}}>
-      <div className="mb-4">        
-        <img className="h-16 w-16 m-auto" src={icon} alt={title}/>
+      <div className="mb-2">
+        <img className="h-32 w-32 m-auto" src={icon} alt={title}/>
       </div>
       <h2 className="text-lg font-bold mb-4 text-center sm:text-2xl">{title}</h2>
       <ul className="mb-8 sm:mb-0">
